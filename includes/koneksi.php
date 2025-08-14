e4a8dd45cddf056b4ae0461d23a3dde281c106b8
SD Negeri Sukokerto 01 <kontak@sdnsukokerto01.sch.id>
2025-08-14 18:44:36 +0700

    Update koneksi.php

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -1,22 +1,20 @@
-<?php
-// file: koneksi.php
-
-$servername = "sql203.infinityfree.com";
-$username = "if0_39696997";
-$password = "Korsa20523318";
-$dbname = "if0_39696997_presensi";
-
-try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    // Untuk production, jangan tampilkan error detail
-    die("Koneksi ke database gagal: " . $e->getMessage());
-}
-
-// Mulai session di sini agar tersedia di semua halaman
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+<?php
+
+// file: koneksi.php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "database";
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    // Untuk production, jangan tampilkan error detail
+    die("Koneksi ke database gagal: " . $e->getMessage());
+}
+// Mulai session di sini agar tersedia di semua halaman
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
